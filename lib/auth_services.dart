a51520494f23f74690f8633c88409b18515d1655
NVG-064 <aryslow92@gmail.com>
2023-12-03 09:45:31 +0700

    Make _auth as final

--- lib/auth_services.dart
+++ lib/auth_services.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class AuthServices {
-  static FirebaseAuth _auth = FirebaseAuth.instance;
+  static final FirebaseAuth _auth = FirebaseAuth.instance;
 
   static Future<void> signOut() async {
     print('Before sign out: ${_auth.currentUser}');
